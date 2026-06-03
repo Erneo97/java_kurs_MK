b6544a0dab990bcbec5bad9336e6d5ef469d54ce
Erneo97 <michalkaniewski1997@gmail.com>
2026-06-03 18:19:10 +0200

    feat: task 3 matrix 2d 3x3 with number from 1 to 9

--- helloWorld/src/main/java/features/PetleZadania.java
+++ helloWorld/src/main/java/features/PetleZadania.java
@@ -16,6 +16,8 @@ public class PetleZadania {
         drawRectangle(weight, height);
 
         drawPiramideint(height);
+
+        task3Matrix3_3();
     }
 
     private static void drawRectangle(int weight, int height) {
@@ -29,9 +31,32 @@ public class PetleZadania {
 
     private static void drawPiramideint(int height) {
         for (int i = 0; i < height; i++) {
-            System.out.println(" ".repeat((height-i)-1) + "*".repeat(i*2+1));
+            System.out.println(" ".repeat((height - i) - 1) + "*".repeat(i * 2 + 1));
         }
     }
 
+
+    static private final int SIZE_ARRAY = 3;
+
+    static private void task3Matrix3_3() {
+        int[][] array = new int[SIZE_ARRAY][SIZE_ARRAY];
+
+        for (int i = 0; i < SIZE_ARRAY; i++) {
+            for (int j = 0; j < SIZE_ARRAY; j++) {
+                array[i][j] = SIZE_ARRAY * i + j + 1;
+            }
+        }
+
+        printArray(array);
+    }
+
+    static private void printArray(int[][] array) {
+        for (int i = 0; i < SIZE_ARRAY; i++) {
+            for (int j = 0; j < SIZE_ARRAY; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 
 }
