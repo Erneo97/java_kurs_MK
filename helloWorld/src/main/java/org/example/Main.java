729dd97fb28793781697412f93ec5e8de690f2ce
Erneo97 <michalkaniewski1997@gmail.com>
2026-06-08 18:14:20 +0200

    revactor: delete warings

--- helloWorld/src/main/java/org/example/Main.java
+++ helloWorld/src/main/java/org/example/Main.java
@@ -3,8 +3,6 @@ package org.example;
 import features.LIFO;
 import features.Point;
 
-import java.util.Locale;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -65,25 +63,6 @@ public class Main {
 
         lifoPoint.remove(new Point(20.0, 30.0));
         System.out.printf("%s  - %d %n", lifoPoint, lifoPoint.size());
-
-//        for (int i = 1; i <= 10 ; i++) {
-//            lifo.pop( );
-//            System.out.printf("%s  - %d %n", lifo, lifo.size() );
-//        }
-
-
-//        double d = 12311145.6789;
-//        String s = String.format("wartosc  %1$,10.2f", d);
-//
-//        System.out.println("Formatted Price: " + s);
-//
-//        double d2 = 11145.6789;
-//        s = String.format(Locale.ITALIAN, "%2$,10.2f \t%1$,10.2f", d2, d); // formatowanie pozwala zmienić separator na kropkę przecinek co 3 cyfry
-//        System.out.printf("Formatted Price: %S%n", s);
-//
-//        for (int i = 0; i < 10; i++) {
-//            System.out.printf("%1$d   -   %1$B%n", i);
-//        }
 
     }
 }
