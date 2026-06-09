09e7d4393b8d265e209470d6f1f3a17cf17309e6
Erneo97 <michalkaniewski1997@gmail.com>
2026-06-09 17:42:08 +0200

    feat: code formatting

--- helloWorld/src/main/java/org/example/Main.java
+++ helloWorld/src/main/java/org/example/Main.java
@@ -84,10 +84,10 @@ public class Main {
         if (n == 0) { // warunek stopu
             return 0;
         }
-        if( n == 1) {
+        if (n == 1) {
             return 1;
         }
-        return fibonacci(n-2) + fibonacci(n - 1);
+        return fibonacci(n - 2) + fibonacci(n - 1);
     }
 
 }
