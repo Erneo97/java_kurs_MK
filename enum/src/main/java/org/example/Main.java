9c0302dff6e2a431f866d1227d57a616e31c34f5
Erneo97 <michalkaniewski1997@gmail.com>
2026-06-10 18:55:59 +0200

    refactor: delete empty lines

--- enum/src/main/java/org/example/Main.java
+++ enum/src/main/java/org/example/Main.java
@@ -1,7 +1,5 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         double base = 100;
@@ -9,7 +7,7 @@ public class Main {
         System.out.println("Bazowa cena: " + base );
         System.out.println("\tCena dla danych biletów");
         for (TicketType type: TicketType.values()) {
-            System.out.printf("%s cena: %8.2f%n", type.getName(), TicketType.calculateTickerPrice(type, base));
+            System.out.printf("%7s cena: %8.2f%n", type.getName(), TicketType.calculateTickerPrice(type, base));
         }
     }
 }
