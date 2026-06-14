b7e878da2c930021d516e136ddbbc98ecb98e796
Erneo97 <michalkaniewski1997@gmail.com>
2026-06-14 21:49:47 +0200

    feat: creating sample objects

---
+++ vehicle/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Vehicle> vehicles = new ArrayList<>();
+        vehicles.add(new Car("Volks Wagen", "Pasat", 2003, FuelType.DISEL, 5));
+        vehicles.add(new Motorcycle("Junak", "M16 125", 2024, FuelType.PETROL));
+        vehicles.add(new Car("Skoda", "Elroq", 2026, FuelType.ELECTRIC, 3));
+
+    }
+}
