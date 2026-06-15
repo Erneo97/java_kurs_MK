ff4f55a38dc48957a573d90b79dda2068b206b20
Erneo97 <michalkaniewski1997@gmail.com>
2026-06-15 16:32:35 +0200

    refactor: remov unused list

--- vehicle/src/main/java/org/example/Main.java
+++ vehicle/src/main/java/org/example/Main.java
@@ -6,9 +6,8 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Vehicle> vehicles = new ArrayList<>();
-        List<Drivable> vehiclesDrivable = new ArrayList<>();
 
-        initLists(vehicles, vehiclesDrivable);
+        initLists(vehicles);
 
         double litersToTank = 15;
         for (Vehicle vehicle : vehicles) {
@@ -23,14 +22,12 @@ public class Main {
         }
     }
 
-    static void initLists(List<Vehicle> vehicles, List<Drivable> vehiclesDrivable) {
+    static void initLists(List<Vehicle> vehicles) {
         Car carVolks = new Car("Volks Wagen", "Pasat", 2003, FuelType.DISEL, 5);
         vehicles.add(carVolks);
-        vehiclesDrivable.add(carVolks);
 
         Motorcycle newMotor = new Motorcycle("Junak", "M16 125", 2024, FuelType.PETROL);
         vehicles.add(newMotor);
-        vehiclesDrivable.add(newMotor);
 
         Car carSkoda = new Car("Skoda", "Elroq", 2026, FuelType.ELECTRIC, 3);
         vehicles.add(carSkoda);
