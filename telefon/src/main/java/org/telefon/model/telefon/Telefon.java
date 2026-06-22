2f19558f65d4396b4a80baa7fb2be4a4b96df427
Erneo97 <michalkaniewski1997@gmail.com>
2026-06-22 19:59:52 +0200

    freat: class telefon basic api for handling calls

--- telefon/src/main/java/org/telefon/model/telefon/Telefon.java
+++ telefon/src/main/java/org/telefon/model/telefon/Telefon.java
@@ -1,11 +1,42 @@
 package org.telefon.model.telefon;
 
+import org.telefon.exeption.PhoneNumberAlredyExistsExeption;
+import org.telefon.model.operator.Operator;
+
 import java.awt.*;
 import java.util.Objects;
 
 public abstract class Telefon{
     protected String interfejsKomunikacyjny;
     protected Color color;
+    Operator operator;
+    boolean conected;
+
+    public Telefon(String interfejsKomunikacyjny, Color color, Operator operator){
+        this.interfejsKomunikacyjny = interfejsKomunikacyjny;
+        this.color = color;
+        this.operator = operator;
+
+        try {
+            operator.dodajTelefon(this);
+            conected = true;
+        } catch (PhoneNumberAlredyExistsExeption e) {
+            conected = false;
+        }
+    }
+
+    private void tryToConnect() {
+        try {
+            operator.dodajTelefon(this);
+            conected = true;
+        } catch (PhoneNumberAlredyExistsExeption e) {
+            conected = false;
+        }
+    }
+
+    public String getInterfejsKomunikacyjny() {
+        return interfejsKomunikacyjny;
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -18,4 +49,6 @@ public abstract class Telefon{
     public int hashCode() {
         return Objects.hash(interfejsKomunikacyjny, color);
     }
+
+    public abstract void odbierz(String numerTelefonu);
 }
