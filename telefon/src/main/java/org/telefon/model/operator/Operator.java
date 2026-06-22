d852b607a533c511a8d0a1e5ebb6e2b04fc0a241
Erneo97 <michalkaniewski1997@gmail.com>
2026-06-22 19:36:02 +0200

    feat: method to login number in operator

--- telefon/src/main/java/org/telefon/model/operator/Operator.java
+++ telefon/src/main/java/org/telefon/model/operator/Operator.java
@@ -1,10 +1,11 @@
 package org.telefon.model.operator;
 
+import org.telefon.exeption.PhoneNumberAlredyExistsExeption;
 import org.telefon.exeption.PhoneNumberIsOfflineExeption;
 import org.telefon.model.telefon.Telefon;
 
 public interface Operator {
-    void dodajTelefon(Telefon telefon) throws PhoneNumberIsOfflineExeption;
+    void dodajTelefon(Telefon telefon) throws PhoneNumberAlredyExistsExeption;
     void usunTelefon(Telefon telefon);
     void dzwonNaNumer(String numer) throws PhoneNumberIsOfflineExeption;
 }
--- telefon/src/main/java/org/telefon/model/operator/OperatorKomorkowy.java
+++ telefon/src/main/java/org/telefon/model/operator/OperatorKomorkowy.java
@@ -1,17 +1,31 @@
 package org.telefon.model.operator;
 
+import org.telefon.exeption.PhoneNumberAlredyExistsExeption;
 import org.telefon.exeption.PhoneNumberIsOfflineExeption;
 import org.telefon.model.telefon.Telefon;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class OperatorKomorkowy implements Operator {
     Map<String, Telefon> telefonyWSieci = new HashMap<>();
 
     @Override
-    public void dodajTelefon(Telefon telefon) throws PhoneNumberIsOfflineExeption {
+    public void dodajTelefon(Telefon telefon) throws PhoneNumberAlredyExistsExeption {
+        Optional<String> optionalFoundNumber = findPhoneNumberOnline(telefon.getInterfejsKomunikacyjny());
+        if(optionalFoundNumber.isPresent()){
+            throw new PhoneNumberAlredyExistsExeption(String.format("Numer telefonu %s istnieje", optionalFoundNumber.get()));
+        }
 
+        telefonyWSieci.put(telefon.getInterfejsKomunikacyjny(), telefon);
+    }
+
+    private Optional<String> findPhoneNumberOnline(String searchedNumber) {
+        return telefonyWSieci.keySet()
+                .stream()
+                .filter(numerTelefonu -> numerTelefonu.equals(searchedNumber))
+                .findAny();
     }
 
     @Override
