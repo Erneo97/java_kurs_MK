7aff6c99b0da678106cf357150f9de24e50f57a1
Erneo97 <michalkaniewski1997@gmail.com>
2026-06-22 21:47:48 +0200

    feat: rand phone numbers

--- telefon/src/main/java/org/telefon/Main.java
+++ telefon/src/main/java/org/telefon/Main.java
@@ -1,7 +1,32 @@
 package org.telefon;
 
+import org.telefon.model.telefon.Telefon;
+
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Main {
+    private static List<Telefon> telefons = new ArrayList<>();
+    private static Set<String> phoneNumbers = new HashSet<>();
+
+    private static void createPhonesNumber(int coutOfNumbers) {
+        while (phoneNumbers.size() < coutOfNumbers) {
+            String number = String.format("%03d-%03d-%03d",
+                    randNumber0_999(),
+                    randNumber0_999(),
+                    randNumber0_999());
+            phoneNumbers.add(number);
+        }
+    }
+
+    private static int randNumber0_999() {
+        return ThreadLocalRandom.current().nextInt(0, 999);
+    }
+
     public static void main(String[] args) {
+        int coutOfTestNumbers = 5;
+        createPhonesNumber(coutOfTestNumbers);
+
 
     }
 }
